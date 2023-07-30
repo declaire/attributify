461ff4d1a08b2e9bfa9b23b59966c92a066dfeeb
Claire <cll009@ucsd.edu>
2023-07-30 00:18:04 -0700

    fixed config

--- config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-
-    CLIENT_ID: 'c9815be2d35041f69712acba82f994b9',
-    CLIENT_SECRET: '7b7db31553ad466e9cac3c24a3890bce',
-    REDIRECT_URI: 'http://localhost:3000/'
-}
-
-module.exports = config;
